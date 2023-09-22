dd519429c9c57a4d7cd5af424fe50f4e67b3a601
k00w <112608822+devkoow@users.noreply.github.com>
2023-09-22 17:52:29 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -8,3 +8,11 @@ document.addEventListener('scroll', function () {
     header.classList.remove('black');
   }
 });
+
+// eventListner 하나당 로직 하나를 가질 수 있게 분리하는게 성능, 가독성면에서 더 좋다.
+// 홈 섹션의 투명도와 스크롤을 적용한 스타일링
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', function () {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
